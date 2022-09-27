9eab2e9ad91e2b1172f0f8535464ca6d490ac243
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-09-27 07:09:32 +0530

    Update Git_commands.bat

--- {{ cookiecutter.repo_name }}/Git_commands.bat
+++ {{ cookiecutter.repo_name }}/Git_commands.bat
@@ -1,5 +1,5 @@
 git init -b main
-git add . && git commit -m "initial commit"
+git add . && git commit -m "First commit"
 gh repo create {{cookiecutter.repo_name}}
 git remote add origin https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.repo_name}}.git
 git remote -v
