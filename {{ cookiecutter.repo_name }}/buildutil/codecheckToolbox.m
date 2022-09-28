c5956fb964f6a11833ecd682eb4a6cf5faa74db9
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-09-28 22:46:11 +0530

    Update codecheckToolbox.m

--- {{ cookiecutter.repo_name }}/buildutil/codecheckToolbox.m
+++ {{ cookiecutter.repo_name }}/buildutil/codecheckToolbox.m
@@ -5,33 +5,36 @@ function codecheckToolbox(rootDir)
 
     testFileInfo = dir(fullfile(rootDir,"test","*.m"));
     filesToCheck = fullfile(string({testFileInfo.folder}'),string({testFileInfo.name}'));
+
+    testFileInfo = dir(fullfile(rootDir,"test","*.mlx"));
+    filesToCheck = [filesToCheck;fullfile(string({testFileInfo.folder}'),string({testFileInfo.name}'))];
     
     testFileInfo = dir(fullfile(rootDir,"code","*.m"));
     filesToCheck = [filesToCheck;fullfile(string({testFileInfo.folder}'),string({testFileInfo.name}'))];
+
+    testFileInfo = dir(fullfile(rootDir,"code","*.mlx"));
+    filesToCheck = [filesToCheck;fullfile(string({testFileInfo.folder}'),string({testFileInfo.name}'))];
     
     if isempty(filesToCheck)
-        error("%d:codeissues",string({{ cookiecutter.repo_name }}),"No files to check.")
+        error("%s:codeissues",string("{{cookiecutter.repo_name}}"),"No files to check.")
+    else
+        issues = checkcode(filesToCheck);
+        issues = [issues{:}];
+        issueCount = size(issues,1);
+        fprintf("checked %d files with %d issue(s).\n",numel(filesToCheck),issueCount)
+        % Generate the JSON files for the shields in the readme.md
+        switch issueCount
+            case 0
+                color = "green";
+            case 1
+                color = "yellow";
+            otherwise
+                color = "red";
+        end
+        writeBadgeJSONFile("code issues",string(issueCount), color)
+        if issueCount ~= 0
+            checkcode(filesToCheck)
+            error("%s:codeissues",string("{{cookiecutter.repo_name}}"),"%s Toolbox requires all code check issues be resolved.",string("{{cookiecutter.repo_name}}"))
+        end
     end
-    issues = checkcode(filesToCheck);
-    issues = [issues{:}];
-    issueCount = size(issues,1);
-
-    fprintf("checked %d files with %d issue(s).\n",numel(filesToCheck),issueCount)
-
-    % Generate the JSON files for the shields in the readme.md
-    switch issueCount
-        case 0
-            color = "green";
-        case 1
-            color = "yellow";
-        otherwise
-            color = "red";
-    end
-    writeBadgeJSONFile("code issues",string(issueCount), color)
-    
-    if issueCount ~= 0
-        checkcode(filesToCheck)
-        error("%d:codeissues",string({{ cookiecutter.repo_name }}),"%d Toolbox requires all code check issues be resolved.",string({{ cookiecutter.repo_name }}))
-    end
-    TestFileTxt()
 end
