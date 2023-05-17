481e676ef92b812ea23e937c2913411efdc95d4a
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2023-05-17 12:02:56 +0530

    Delete {{ cookiecutter.rootFolderName }}/{{ cookiecutter.rootFolderName }}Toolbox directory

--- {{ cookiecutter.rootFolderName }}/{{ cookiecutter.rootFolderName }}Toolbox/addTwoVariable.m
+++
@@ -1,4 +0,0 @@
-function c = addTwoVariable(a,b)    
-%% This Function add two variables
-    c = a+b;
-end
--- {{ cookiecutter.rootFolderName }}/{{ cookiecutter.rootFolderName }}Toolbox/subTwoVariable.m
+++
@@ -1,4 +0,0 @@
-function c = subTwoVariable(a,b)    
-%% This Function subtracts two variables
-    c = a-b;
-end
