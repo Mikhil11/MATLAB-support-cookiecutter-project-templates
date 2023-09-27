a4ee5ef953de346ba55a4b0ba4a8c6c76868fb28
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2023-09-27 11:19:41 +0530

    Create addVariableExample.m

---
+++ {{ cookiecutter.RootFolderName }}/toolbox/examples/addVariableExample.m
@@ -0,0 +1,15 @@
+% ADDVARIABLE - Add two variables
+%
+%   c = addVariable(a, b) adds the values of variables a and b and returns the result.
+%
+%   Inputs:
+%       a - First variable
+%       b - Second variable
+%
+%   Output:
+%       c - Result of adding a and b
+%
+%   Example:
+%       c = addVariable(2, 3) returns c = 5.
+%
+%   See also: subtractVariable
